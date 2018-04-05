39880ef071fb5b08d0f3381e9f49990bc7341a41
erikav <erikav@erikav-X541UAK>
2018-04-04 20:38:35 -0600

    datos

---
+++ src/datosgenerales/Datosgenerales.java
@@ -0,0 +1,11 @@
+/* Autor:Erika Rodriguez*/
+package datosgenerales;
+
+public class Datosgenerales {
+
+     
+    public static void main(String[] args) {
+        System.out.println("Nombre:Erika Liliana Rodriguez \nCarnet:00106416 \nProfesion:estudiante de ing. informatica \nFecha de nacimiento: 22 de Marzo de 1998 \nEstado civil: Soltera \nLugar de estudio:Universidad Centroamericana Jose Simeon Cañas \nEdad:20");
+    }
+    
+}
